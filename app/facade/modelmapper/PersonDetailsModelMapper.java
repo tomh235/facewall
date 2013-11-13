25dbdc90174f3a3d9e94d737e5b5d768282d9b72
Charlie Briggs <charles.briggs@telefonica.com>
2013-11-13 15:53:57 +0000

    [CB] refactored SearchResultsModelMapper as Java

---
+++ app/facade/modelmapper/PersonDetailsModelMapper.java
@@ -0,0 +1,10 @@
+package facade.modelmapper;
+
+import domain.Person;
+import model.PersonDetailsModel;
+
+public class PersonDetailsModelMapper {
+    public PersonDetailsModel map(Person person){
+        return new PersonDetailsModel(person.name());
+    }
+}
--- app/facade/modelmapper/PersonDetailsModelMapper.scala
+++
@@ -1,8 +0,0 @@
-package facade.modelmapper
-
-import model.PersonDetailsModel
-import domain.Person
-
-class PersonDetailsModelMapper {
-    def map(person: Person): PersonDetailsModel = new PersonDetailsModel(person.name)
-}
