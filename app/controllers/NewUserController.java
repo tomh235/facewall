12cafb5e81503e0218bec2f8be378ee74c89393b
Hugo <hwainwright@gmx.com>
2013-11-18 20:58:52 +0000

    [HW] -- fixed merge breakage in NewUserController - the Scala facewall repo is now called FacewallScalaRepo -- Added neo4j rest binding as a dependency -- renamed gen-idea script to play-idea

--- app/controllers/NewUserController.java
+++ app/controllers/NewUserController.java
@@ -1,18 +1,14 @@
 package controllers;
 
-import model.User;
+import data.FacewallScalaRepo;
 import facade.OverviewFacade;
-import play.api.templates.Html;
-import play.data.DynamicForm;
+import model.User;
 import play.data.Form;
-import play.mvc.Action;
 import play.mvc.Controller;
-import play.mvc.Http;
 import play.mvc.Result;
-import repository.FacewallRepo;
-import views.html.*;
+import views.html.newuser;
+import views.html.overview;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,7 +16,7 @@ import static play.data.Form.form;
 
 public class NewUserController extends Controller {
 
-    private static FacewallRepo repo = new FacewallRepo();
+    private static FacewallScalaRepo repo = new FacewallScalaRepo();
     private static OverviewFacade overviewFacade = new OverviewFacade(repo);
     private static Form<User> userForm = form(User.class);
     private static Map<String,Object> formData = new HashMap();
