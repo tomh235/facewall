398f1c3aa5830c236e040068154a2dc6939f0868
Luke Langfield <luke.langfield@o2.com>
2013-11-25 13:54:03 +0000

    [LL]sign up facade test and sign up facade

---
+++ app/facade/SignupFacade.java
@@ -0,0 +1,10 @@
+package facade;
+
+import model.User;
+
+public class SignupFacade {
+       public void storeNewUser(User user){
+
+
+       }
+}
---
+++ test/facade/SignupFacadeTest.java
@@ -0,0 +1,11 @@
+package facade;
+
+
+import org.junit.Test;
+
+public class SignupFacadeTest {
+    @Test
+    public void when_storeuser_is_called_that_user_is_sent_to_repository(){
+
+    }
+}
