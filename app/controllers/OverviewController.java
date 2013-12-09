1436c8deb364dd8e44e6a108b206d3fa602b9c7e
Charlie Briggs <charles.briggs@telefonica.com>
2013-12-09 22:01:40 +0000

    [CB] -- actually split up OverviewAndSearchController

--- app/controllers/OverviewController.java
+++ app/controllers/OverviewController.java
@@ -1,7 +1,16 @@
 package controllers;
 
-/**
- * Created by charlie on 09/12/13.
- */
-public class OverviewController {
+import data.FacewallScalaRepo;
+import data.ScalaRepository;
+import facade.OverviewFacade;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class OverviewController extends Controller {
+    private static final ScalaRepository repo = new FacewallScalaRepo();
+    private static final OverviewFacade overviewFacade = new OverviewFacade(repo);
+
+    public static Result overview() {
+        return ok(views.html.overview.render(overviewFacade.createOverviewModel()));
+    }
 }
--- app/controllers/SearchController.java
+++ app/controllers/SearchController.java
@@ -3,7 +3,6 @@ package controllers;
 import data.FacewallScalaRepo;
 import data.ScalaRepository;
 import domain.Query;
-import facade.OverviewFacade;
 import facade.SearchFacade;
 import facade.modelmapper.PersonDetailsModelMapper;
 import facade.modelmapper.SearchResultsModelMapper;
@@ -16,16 +15,10 @@ import play.mvc.Controller;
 import play.mvc.Result;
 import requestmapper.SearchQueryMapper;
 
-//Split these up into two controllers
 public class SearchController extends Controller {
     private static final ScalaRepository repo = new FacewallScalaRepo();
     private static final SearchQueryMapper searchQueryMapper = new SearchQueryMapper();
-    private static final OverviewFacade overviewFacade = new OverviewFacade(repo);
     private static final SearchFacade searchFacade = new SearchFacade(repo, new SearchResultsModelMapper(), new PersonDetailsModelMapper(), new TeamDetailsModelMapper());
-
-    public static Result overview() {
-        return ok(views.html.overview.render(overviewFacade.createOverviewModel()));
-    }
 
     public static Result search() {
         return ok(views.html.search.render());
