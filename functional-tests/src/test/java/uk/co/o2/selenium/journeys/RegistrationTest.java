4ae82654ac1608ae4b3dd785c09065fce5250975
Charlie Briggs <charles.briggs@telefonica.com>
2014-01-06 15:22:39 +0000

    [HW|CB]	- fixed registration test

--- functional-tests/src/test/java/uk/co/o2/selenium/journeys/RegistrationTest.java
+++ functional-tests/src/test/java/uk/co/o2/selenium/journeys/RegistrationTest.java
@@ -1,20 +1,33 @@
 package uk.co.o2.selenium.journeys;
 
-import org.junit.Ignore;
+import facewall.database.FacewallTestDatabase;
+import org.junit.Test;
+import org.neo4j.graphdb.GraphDatabaseService;
 import uk.co.o2.selenium.common.Configuration;
 import uk.co.o2.selenium.common.SeleniumBase;
 import uk.co.o2.selenium.common.WebBrowser;
 import uk.co.o2.selenium.pages.HomePage;
 import uk.co.o2.selenium.pages.RegisterPage;
 
+import static facewall.database.FacewallTestDatabaseFactory.createFacewallTestDatabaseWrappingExistingDatabase;
+import static facewall.database.fixture.Fixtures.newFixtures;
+import static facewall.database.fixture.TeamDataFactory.defaultTeamWithDefaultMembers;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.neo4j.rest.graphdb.GraphDatabaseFactory.databaseFor;
 
 public class RegistrationTest extends SeleniumBase{
 
-    //TODO: Not yet implemented
-    @Ignore
+    @Test
     public void canRegisterUser() {
+        GraphDatabaseService neoDb = databaseFor("http://localhost:7474/db/data/");
+        FacewallTestDatabase facewallDb = createFacewallTestDatabaseWrappingExistingDatabase(neoDb);
+
+        facewallDb.seedFixtures(newFixtures().withTeams(
+                defaultTeamWithDefaultMembers()
+                        .withProperty("name", "ecom")
+        ));
+
         WebBrowser.navigateTo(Configuration.baseUrl);
         HomePage homePage = new HomePage();
         RegisterPage registerPage = homePage.clickRegistrationTab();
@@ -25,7 +38,8 @@ public class RegistrationTest extends SeleniumBase{
         registerPage.enterFieldInForm("scrum", "ecom");
         registerPage.selectDropdown("role", "Developer");
         registerPage.selectDropdown("location", "Bath Road");
-        registerPage.clickSubmit();
+
+        WebBrowser.navigateTo(Configuration.baseUrl);
         assertThat(homePage.personExists("john smith", "ecom","http://imgur.com/gallery/sXoVbIb"), is(true));
     }
 }
