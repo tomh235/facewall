ee24f6b4b1f204851c09f189f54330f28e3ab0dd
Hugo <hwainwright@gmx.com>
2014-02-03 12:42:58 +0000

    [HW] -- fixed errors from removing play classes

--- app/model/UserModel.java
+++ app/model/UserModel.java
@@ -1,37 +1,12 @@
 package model;
 
-import org.apache.commons.lang.WordUtils;
-import org.hibernate.validator.constraints.URL;
-import play.data.validation.Constraints.Email;
-import play.data.validation.Constraints.MaxLength;
-import play.data.validation.Constraints.Required;
-
 public class UserModel {
-    @Required(message = "An Email address is required")
-    @Email(message = "Please enter a valid email")
-    @MaxLength(value = 50, message = "Maximum email length is 50 characters")
+
     public String email;
-
-    @Required(message = "An image URL is a required")
-    @URL(message = "Please enter a valid URL")
     public String imgURL;
-
-    @Required(message = "An Email is required")
-    @MaxLength(value = 50, message = "Maximum name length is 50 characters")
     public String name;
-
-    @Required(message = "A Role is required")
     public String role;
-
-    @Required(message = "A Location is required")
     public String location;
-
-    @Required(message = "A Team is required")
     public String team;
     public String scrum;
-
-    public String prettifyString(String inputString) {
-        String str1 = inputString.replaceAll("_", " "); //Remove underscores
-        return WordUtils.capitalizeFully(str1); //Capitalise words
-    }
 }
