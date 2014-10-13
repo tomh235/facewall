4c511160e015837662409a8092cf12abbeb0fa77
tholton235 <thomas.holton@telefonica.com>
2014-10-13 15:06:18 +0100

    updated Readme with better instructions, updated browser config to get selenium working

--- functional-tests/src/test/java/uk/co/o2/facewall/functionaltests/selenium/common/Configuration.java
+++ functional-tests/src/test/java/uk/co/o2/facewall/functionaltests/selenium/common/Configuration.java
@@ -6,5 +6,5 @@ public class Configuration {
     public static String browser = System.getProperty("qa.browser", "firefox");
     public static String browserVersion = System.getProperty("qa.browserVersion", "22");
 
-    public static String baseUrl = "http://localhost:9001/facewall";
+    public static String baseUrl = "http://localhost:9000/facewall";
 }
