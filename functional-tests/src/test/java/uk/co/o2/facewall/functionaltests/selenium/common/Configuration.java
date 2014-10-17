9df1e98792cefcf3b24ae6b85344fcb25d61d491
Tom Cable <tcable1.o2@gmail.com>
2014-10-17 09:59:02 +0100

    Changed the version of Firefox in the Configuration.java

--- functional-tests/src/test/java/uk/co/o2/facewall/functionaltests/selenium/common/Configuration.java
+++ functional-tests/src/test/java/uk/co/o2/facewall/functionaltests/selenium/common/Configuration.java
@@ -4,7 +4,7 @@ public class Configuration {
 
     public static String testDriver = System.getProperty("qa.testdriver", "webdriver"); // "jsoup" or "webdriver"
     public static String browser = System.getProperty("qa.browser", "firefox");
-    public static String browserVersion = System.getProperty("qa.browserVersion", "22");
+    public static String browserVersion = System.getProperty("qa.browserVersion", "31");
 
     public static String baseUrl = "http://localhost:9000/facewall";
 }
